1e5d04e0ffc8f2607c5cf698c725b1de4b6f34a3
Elon Park <sungwoon.park92@gmail.com>
2020-10-16 20:53:35 +0900

    :sparkles: Add AttributedStringSugar.xcodeproj forCarthage

---
+++ AttributedStringSugar/AttributedStringSugar.h
@@ -0,0 +1,19 @@
+//
+//  AttributedStringSugar.h
+//  AttributedStringSugar
+//
+//  Created by Elon on 2020/10/16.
+//  Copyright © 2020 ElonPark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AttributedStringSugar.
+FOUNDATION_EXPORT double AttributedStringSugarVersionNumber;
+
+//! Project version string for AttributedStringSugar.
+FOUNDATION_EXPORT const unsigned char AttributedStringSugarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttributedStringSugar/PublicHeader.h>
+
+
